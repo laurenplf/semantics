c9ab5bd5f7e5601777cdd40ae9f30d38ca7c5176
laurenplf <pierre-louis.laurent@depinfonancy.ney>
2019-05-27 10:08:37 +0200

    Fin code prof

--- nanoC.py
+++ nanoC.py
@@ -1,24 +1,67 @@
-from sly import Lexer
+from sly import Lexer, Parser
 import sys
 
 class NanoCLexer(Lexer):
 
-    tokens = { PLUS, MOINS, ID }
+    tokens = { OPBIN, ID, WHILE, MAIN, IF, NOMBRE, LBRACE, RBRACE, LPAREN, RPAREN,
+               SEMICOLON, COMMA, EQUAL, LT }
+    #On veut aussi ajouter les signes suivants : ( ) { } ; , = <
 
     ignore = ' \t\n'
 
-    ID = r'[a-z]+'
-    PLUS = r'\+'
-    MOINS = r'-'
+    OPBIN = r'[\+-]'
+    WHILE = r'while'
+    IF = r'if'
+    MAIN = r'main'
+    NOMBRE = r'[0-9]+'
+    LBRACE = r'\{'
+    RBRACE = r'\}'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    SEMICOLON = r'\;'
+    COMMA = r'\,'
+    EQUAL = r'\='
+    LT = r'\<'
+
+
+    @_(r'[a-z]+')
+    def ID(self, t):
+        return t
 
     def error(self, t):
         print("error at %s" % t)
         sys.exit(1)
 
 
+programme = '''x, z'''
 lexer = NanoCLexer()
-toks = lexer.tokenize('''qsjd ++
-12-
-qsdkh''')
-for tok in toks:
-    print(tok)
+toks = lexer.tokenize(programme)
+
+class NanoCParser(Parser):
+
+    tokens = lexer.tokens
+
+    @_('ID')
+    def varlist(self, p):
+        return (p[0],)
+
+    @_('ID COMMA varlist')
+    def varlist(self, p):
+        return (p[0],) + p[2]
+
+    @_('expr OPBIN expr')
+    def expr(self, p):
+        return p
+
+    @_('NOMBRE')
+    def expr(self, p):
+        return p
+
+    @_('ID')
+    def expr(self, p):
+        return p
+
+
+parser = NanoCParser()
+x = parser.parse(lexer.tokenize(programme))
+print("x = %s" % str(x))
