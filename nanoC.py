41e27817442d9431dee8d315e757e45ba3a2d6cf
camille-sivelle <40430753+camille-sivelle@users.noreply.github.com>
2019-05-27 11:24:18 +0200

    Update nanoC.py

--- nanoC.py
+++ nanoC.py
@@ -33,55 +33,7 @@ class NanoCLexer(Lexer):
         sys.exit(1)
 
 
-programme = '''x, z'''
-lexer = NanoCLexer()
-toks = lexer.tokenize(programme)
-
-class NanoCParser(Parser):
-
-    tokens = lexer.tokens
-
-    @_('ID')
-    def varlist(self, p):
-        return (p[0],)
-
-    @_('ID COMMA varlist')
-    def varlist(self, p):from sly import Lexer, Parserfrom sly import Lexer, Parser
-import sys
-
-class NanoCLexer(Lexer):
-
-    tokens = { OPBIN, ID, WHILE, MAIN, IF, NOMBRE, LBRACE, RBRACE, LPAREN, RPAREN,
-               SEMICOLON, COMMA, EQUAL, LT }
-    #On veut aussi ajouter les signes suivants : ( ) { } ; , = <
-
-    ignore = ' \t\n'
-
-    OPBIN = r'[\+-]'
-    WHILE = r'while'
-    IF = r'if'
-    MAIN = r'main'
-    NOMBRE = r'[0-9]+'
-    LBRACE = r'\{'
-    RBRACE = r'\}'
-    LPAREN = r'\('
-    RPAREN = r'\)'
-    SEMICOLON = r'\;'
-    COMMA = r'\,'
-    EQUAL = r'\='
-    LT = r'\<'
-
-
-    @_(r'[a-z]+')
-    def ID(self, t):
-        return t
-
-    def error(self, t):
-        print("error at %s" % t)
-        sys.exit(1)
-
-
-programme = '''a = 1'''
+programme = '''a = 1;'''
 lexer = NanoCLexer()
 toks = lexer.tokenize(programme)
 
@@ -99,7 +51,7 @@ class NanoCParser(Parser):
     
     @_('ID EQUAL expr SEMICOLON')
     def instr(self, p):
-        return p 
+        return ('affect', p[0],p[2])
     
     @_('IF expr LBRACE instr RBRACE')
     def instr(self, p):
@@ -132,111 +84,5 @@ parser = NanoCParser()
 for t in lexer.tokenize(programme):
     print(t)
     
-x = parser.parse(lexer.tokenize(programme))
-print("x = %s" % str(x))
-import sys
-
-class NanoCLexer(Lexer):
-
-    tokens = { OPBIN, ID, WHILE, MAIN, IF, NOMBRE, LBRACE, RBRACE, LPAREN, RPAREN,
-               SEMICOLON, COMMA, EQUAL, LT }
-    #On veut aussi ajouter les signes suivants : ( ) { } ; , = <
-
-    ignore = ' \t\n'
-
-    OPBIN = r'[\+-]'
-    WHILE = r'while'
-    IF = r'if'
-    MAIN = r'main'
-    NOMBRE = r'[0-9]+'
-    LBRACE = r'\{'
-    RBRACE = r'\}'
-    LPAREN = r'\('
-    RPAREN = r'\)'
-    SEMICOLON = r'\;'
-    COMMA = r'\,'
-    EQUAL = r'\='
-    LT = r'\<'
-
-
-    @_(r'[a-z]+')
-    def ID(self, t):
-        return t
-
-    def error(self, t):
-        print("error at %s" % t)
-        sys.exit(1)
-
-
-programme = '''a = 1'''
-lexer = NanoCLexer()
-toks = lexer.tokenize(programme)
-
-class NanoCParser(Parser):
-
-    tokens = lexer.tokens
-    #start = 'varlist'
-    @_('instr SEMICOLON instr')
-    def instr(self, p):
-        return p
-    
-    @_('WHILE expr LBRACE instr RBRACE')
-    def instr(self, p):
-        return p
-    
-    @_('ID EQUAL expr')
-    def instr(self, p):
-        return p 
-    
-    @_('IF expr LBRACE instr RBRACE')
-    def instr(self, p):
-        return p 
-    
-    
-    @_('ID')
-    def varlist(self, p):
-        return (p[0],)
-
-    @_('ID COMMA varlist')
-    def varlist(self, p):
-        return (p[0],) + p[2]
-
-    @_('expr OPBIN expr')
-    def expr(self, p):
-        return p
-
-    @_('NOMBRE')
-    def expr(self, p):
-        return p
-
-    @_('ID')
-    def expr(self, p):
-        return p
-     
-    
-
-
-parser = NanoCParser()
-for t in lexer.tokenize(programme):
-    print(t)
-    
-x = parser.parse(lexer.tokenize(programme))
-print("x = %s" % str(x))
-        return (p[0],) + p[2]
-
-    @_('expr OPBIN expr')
-    def expr(self, p):
-        return p
-
-    @_('NOMBRE')
-    def expr(self, p):
-        return p
-
-    @_('ID')
-    def expr(self, p):
-        return p
-
-
-parser = NanoCParser()
 x = parser.parse(lexer.tokenize(programme))
 print("x = %s" % str(x))
